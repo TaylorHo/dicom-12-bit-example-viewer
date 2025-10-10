29377c3e1de57f54dd801300f7ae00fadd42a033
Taylor Hoffmann <taylor@hotay.dev>
2025-10-10 09:04:15 -0300

    feat: initial project structure

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+
+export default defineConfig({
+  plugins: [viteCommonjs()],
+  optimizeDeps: {
+    exclude: ['@cornerstonejs/dicom-image-loader'],
+    include: ['dicom-parser'],
+  },
+  worker: {
+    format: 'es',
+  },
+});
+
